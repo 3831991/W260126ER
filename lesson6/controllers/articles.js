ed31c30c5a2e0f022600f87bbe9f7c64f2100b61
Your Name <you@example.com>
2026-07-16 19:08:30 +0300

    added

--- lesson6/controllers/articles.js
+++ lesson6/controllers/articles.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { Router } from 'express';
 
 const schema = new mongoose.Schema({
-    addedTime: Date,
+    addedTime: { type: Date, default: Date.now },
     publishDate: Date,
     headline: String,
     description: String,
@@ -17,6 +17,22 @@ const router = Router();
 router.get("/", async (req, res) => {
     const data = await Article.find();
     res.send(data);
+});
+
+router.post("/", async (req, res) => {
+    const { publishDate, headline, description, content, imgUrl } = req.body;
+
+    const obj = new Article({
+        publishDate,
+        headline,
+        description,
+        content,
+        imgUrl,
+    });
+
+    const article = await obj.save();
+
+    res.send(article);
 });
 
 export default router;
