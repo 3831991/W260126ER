3dd2f4b8b916cce26089fde27d5ad7aec13604c4
Your Name <you@example.com>
2026-07-16 18:36:05 +0300

    added

--- lesson6/controllers/articles.js
+++ lesson6/controllers/articles.js
@@ -1,2 +1,15 @@
 import mongoose from "mongoose";
+import { Router } from 'express';
 
+const schema = new mongoose.Schema({
+    addedTime: Date,
+    publishDate: Date,
+    headline: String,
+    description: String,
+    content: String,
+    imgUrl: String,
+});
+
+const Article = mongoose.model("articles", schema);
+
+export const router = Router();
