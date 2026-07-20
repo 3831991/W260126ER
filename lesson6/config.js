66abb5890fe05140c8e3457c6d0e1d3518fe8cd9
Your Name <you@example.com>
2026-07-20 21:00:02 +0300

    added

---
+++ lesson6/config.js
@@ -0,0 +1,1 @@
+export const JWT_SECRET = "W260126ER_THIS_IS_MY_SECRET_lkjfdslkjflksdj789798fsd7978y^&*%&%";
--- lesson6/controllers/auth.js
+++ lesson6/controllers/auth.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
 import { Router } from 'express';
+import { JWT_SECRET } from "../config.js";
 
 const schema = new mongoose.Schema({
     firstName: String,
@@ -35,7 +37,28 @@ router.post("/signup", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
 
+    const userFind = await User.findOne({ email });
+
+    if (!userFind) {
+        return res.status(401).send({ message: "email or password incorrect" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, userFind.password);
+
+    if (!passwordMatch) {
+        return res.status(401).send({ message: "email or password incorrect" });
+    }
+
+    const obj = {
+        userId: userFind._id,
+        fullName: `${userFind.firstName} ${userFind.lastName}`,
+    }
+
+    const token = jwt.sign(obj, JWT_SECRET, { expiresIn: '15m' });
+
+    res.send(token);
 });
 
 export default router;
