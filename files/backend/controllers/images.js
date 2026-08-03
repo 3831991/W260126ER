0d4973b6ef2a44fdda1859ba40bd1f9da76673f7
Your Name <you@example.com>
2026-08-03 20:30:35 +0300

    added

--- files/backend/controllers/images.js
+++ files/backend/controllers/images.js
@@ -1,11 +1,43 @@
 import { Router } from 'express';
 import fs from 'fs';
+import { formidable } from 'formidable';
 
 const router = Router();
 
 router.get("/", (req, res) => {
     fs.readdir('./images', (err, files) => {
         res.send(files);
+    });
+});
+
+router.post("/upload", (req, res) => {
+    const form = formidable();
+
+    form.parse(req, (err, fields, files) => {
+        const file = files.image[0];
+
+        const allowTypes = [
+            'image/jpeg',
+            'image/jpg',
+            'image/png',
+            'image/gif',
+        ];
+
+        if (!allowTypes.includes(file.mimetype)) {
+            return res.status(403).send({ message: "מה הקטע?????" });
+        }
+
+        if (file.size > 1000000 * 2) { // 2MB
+            return res.status(403).send({ message: "לא הגזמנו???" });
+        }
+
+        fs.copyFile(file.filepath, `./images/${file.originalFilename}`, err => {
+            if (err) {
+                console.log(err);
+            }
+
+            res.end();
+        });
     });
 });
 
