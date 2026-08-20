5f3326b1afbcd54712e8f94e46ac5aa8f9920373
Your Name <you@example.com>
2026-08-20 18:14:15 +0300

    added

--- snake/script.js
+++ snake/script.js
@@ -1,5 +1,9 @@
 const height = 30;
 const width = 30;
+const length = 8;
+const snake = new Array(length).fill().map((x, i) => i);
+snake.reverse();
+const head = snake[0];
 const board = document.querySelector(".board");
 board.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
 
@@ -10,4 +14,12 @@ function createBoard() {
     }
 }
 
+function showSnake() {
+    snake.forEach(n => {
+        board.children[n].classList.add("active");
+        
+    });
+}
+
 createBoard();
+showSnake();
